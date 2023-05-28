266f82ffb1378568b25ba6cfbd6831d3ab1251af
kodruck <jonathan.salazar@ucrso.info>
2023-05-28 11:15:05 -0600

    JON-B97126 login with gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, path_names: {sign_in: 'login',singn_out: 'logout',
+                                  password:'secret',confirmation: 'verificacion', 
+                                  unblock: 'unblock',registration: 'register', sign_up: 'sign_up'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
