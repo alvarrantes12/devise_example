02c346cfcfd57176ad85191ca437056b4ea4d715
status <z0d80a@gmail.com>
2023-05-29 16:52:07 -0600

    DARA-C16247 devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', 
+                                    confirmation: 'verification', unblock: 'unblock', 
+                                    registration: 'register', sign_up: 'sign_up'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
